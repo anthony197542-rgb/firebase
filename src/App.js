ff7eea3715a1c805e1031d64ac67e69340373a74
Your Name <anthony197542@gmail.com>
2026-01-16 15:54:26 -0500

    2com

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React from 'react';
 import './App.css';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './firebase/init';
+import { 
+  createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged,
+ } from 'firebase/auth';
+
 
 function App() {
+  const [user, setUser] = React.useState({});
+ const [loading, setLoading] = React.useState(true);
+
+
+  React.useEffect(() => {
+   onAuthStateChanged(auth, (user) => {
+    setLoading(false);
+     console.log(user);
+     if (user) {
+      setUser(user);
+     }
+    })
+  }, []);
   function register() {
     createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
       .then((user) => {
@@ -12,14 +32,31 @@ function App() {
         console.log(error);
       })
   }
+function login() {
+signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
+ .then(({user}) => {
+  console.log(user);
+  setUser(user);
+       //setUser(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      })
+}
+function logout() {
+  signOut(auth);
+setUser({});
 
-  function NavigatorLogin() {
-    
-  }
+}
+ 
   
   return (
     <div className="App">
       <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'loading...' : user.email}
+
     </div>
   );
 }
